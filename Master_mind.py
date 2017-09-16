513838239ed919f3be313c6af07df563452763be
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-16 17:19:03 +0700

    Only real man can use it

--- Master_mind.py
+++ Master_mind.py
@@ -1,3 +1,11 @@
+def conv(n):
+    n = str(n)
+    dic = {'1':"one" , '2':"two" , 
+        '3':"three" , '4':"four" , 
+        '5':"five" , '6':"six" , 
+        '7':"seven" , '8':"eight" , 
+        '9':"nine" , '0':"no"}
+    return dic[n]
 ans = str(input("Enter a target (4-digit integer): "))
 ans = '0'*(4-len(ans)) + ans        #Add 0 if ans < 1000
 g = -1
@@ -11,6 +19,6 @@ if (g != ans):
         elif g[i] in ans:
             o += 1
     if x != 4:
-     print('%d position correct, %d digits correct' % (x,o))
+     print('%s position correct, %s digits correct' % (conv(x),conv(o)))
     if x == 4:
      print('Congratulations, you just mastered my mind!!')
