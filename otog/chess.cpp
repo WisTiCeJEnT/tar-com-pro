2ef9089043d7a7506300222698a95eb70c2fa25c
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-30 23:08:15 +0700

    bugg

--- otog/chess.cpp
+++ otog/chess.cpp
@@ -4,62 +4,29 @@ int main()
 {
     int n,a,b;
     cin >> n >> a >> b;
+    int ans = 0;
     char t[n][n];
     for(int i=0;i<n;i++)
     {
-        if((i/a)%2==0)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if((j/a)%2==0)
-                    t[i][j] = '#';
-                else
-                    t[i][j] = '.';
-            }
-        }
-        else
-        {
-            for(int j=0;j<n;j++)
-            {
-                if((j/a)%2==0)
-                    t[i][j] = '.';
-                else
-                    t[i][j] = '#';
-            }
-        }
-    }
-    char t2[n][n];
-    for(int i=0;i<n;i++)
-    {
-        if((i/b)%2==0)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if((j/b)%2==0)
-                    t2[i][j] = '#';
-                else
-                    t2[i][j] = '.';
-            }
-        }
-        else
-        {
-            for(int j=0;j<n;j++)
-            {
-                if((j/b)%2==0)
-                    t2[i][j] = '.';
-                else
-                    t2[i][j] = '#';
-            }
-        }
-    }
-    int ans = 0;
-    for(int i=0;i<n;i++)
-    {
+        int add = 0;
         for(int j=0;j<n;j++)
         {
-            if (t[i][j] == '#' || t2[i][j] =='#')
-                ans ++;
+            if(((j/a)%2==0 and (i/a)%2==0) or ((j/a)%2!=0 and (i/a)%2!=0))
+                add = 1;
+            if(((j/b)%2==0 and (i/b)%2==0) or ((j/b)%2!=0 and (i/b)%2!=0))
+                add = 1;
+            ans += add;
+            if(add)
+                t[i][j] = '#';
+            else
+                t[i][j] = '.';
         }
     }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout << t[i][j];
+        }
+        cout << endl;
+    } 
     cout << ans << endl;
 }
