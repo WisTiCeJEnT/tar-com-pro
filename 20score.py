7e46e176c6d88099477ff0a485942fb36ba3c9fa
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-17 20:53:14 +0700

    GG EZ

---
+++ 20score.py
@@ -0,0 +1,13 @@
+lis = []
+his = [0,0,0,0,0,0,0,0,0,0,0]
+for i in range(20):
+    inp = int(input("Enter score: "))
+    if 0 <= inp <= 10:
+        his[inp] += 1
+        lis.append(inp)
+    else:
+        print("Score is out of range.")
+print("Original list:")
+print(lis)
+for i in range(11):
+    print("{} ".format(i)+"*"*his[i])
