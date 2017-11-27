65dec6c17ebb73db4c8a1be610524cbffa698560
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-27 13:36:44 +0700

    more condition

--- sumOfAll.py
+++ sumOfAll.py
@@ -1,7 +1,10 @@
 lis = []
 inp = int(input("Enter a number (0 to end): "))
 while inp!=0:
-    lis.append(inp)
+    if 1<=inp<=999:
+        lis.append(inp)
+    else:
+        print("Number is out of range.")
     inp = int(input("Enter a number (0 to end): "))
 print("Original list:")
 print(lis)
