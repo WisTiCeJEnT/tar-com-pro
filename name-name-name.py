5a8f94ee84ec28f752d3888e8edfb30834489404
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-10 18:22:24 +0700

    del comment

--- name-name-name.py
+++ name-name-name.py
@@ -4,14 +4,11 @@ def namelist(lis):
         for s in lis:
             ans += s
         return ans
-    #print(lis[0])
     ans += lis[0]
     i = 1
     while i<len(lis)-1:
-        #print(f", '{lis[i]}'",end='')
         ans += f", {lis[i]}"
         i += 1
-    #print(f" & {lis[i]}")
     ans += f" & {lis[i]}"
     return ans
 import ast
