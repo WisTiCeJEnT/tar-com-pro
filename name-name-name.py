3a7363b4aa22323fb99426c0bd32d49b65e2efde
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-10 18:25:50 +0700

    more readable

--- name-name-name.py
+++ name-name-name.py
@@ -5,11 +5,9 @@ def namelist(lis):
             ans += s
         return ans
     ans += lis[0]
-    i = 1
-    while i<len(lis)-1:
+    for i in range(1,len(lis)-1):
         ans += f", {lis[i]}"
-        i += 1
-    ans += f" & {lis[i]}"
+    ans += f" & {lis[len(lis)-1]}"
     return ans
 import ast
 names = ast.literal_eval(input())
