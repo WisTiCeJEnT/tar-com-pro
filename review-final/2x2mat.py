07a93f540298be1e49e3caaa3cc470b112531f24
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-13 12:56:52 +0700

    add comment

--- review-final/2x2mat.py
+++ review-final/2x2mat.py
@@ -1,10 +1,10 @@
-lis = []
-lis.append(list(map(int,input().split())))
-lis.append(list(map(int,input().split())))
-summ = lis[0][0] + lis[0][1] + lis[1][0] + lis[1][1]
+lis = []            #prepare list for 2x2 matrix
+lis.append(list(map(int,input().split())))  #add row 0 to mat
+lis.append(list(map(int,input().split())))  #add row 1 to mat
+summ = lis[0][0] + lis[0][1] + lis[1][0] + lis[1][1] #sum of first 2x2 on the left #!!! It's gonna error here if the input is less than 2x2!!!
 ans = summ
 for i in range(2,len(lis[0])):
-    summ = summ -lis[0][i-2] -lis[1][i-2]
-    summ = summ +lis[0][i] +lis[1][i]
-    ans = max(ans,summ)
+    summ = summ - lis[0][i-2] - lis[1][i-2] #minus 1 column on the left
+    summ = summ + lis[0][i] + lis[1][i]     #add 1 new col on the right
+    ans = max(ans,summ)         #if new 2x2 mat is greater than last so update it
 print(ans)
