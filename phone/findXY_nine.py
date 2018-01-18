6c3b7b5fb102e6c14b090376b7e64761f2c3e816
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-01-18 11:24:17 +0700

    basic command only

--- phone/findXY_nine.py
+++ phone/findXY_nine.py
@@ -1,10 +1,12 @@
 a1,b1,c1 = map(float,input().split())
 a2,b2,c2 = map(float,input().split())
-try:
+#try:
+if a1 != 0:
     y = ((a2/a1)*c1 - c2) / (b2 - (a2/a1)*b1)
     x = -(b1*y + c1) / a1
     print(x,y)
-except ZeroDivisionError:
+#except ZeroDivisionError:
+else:
     if b1 != 0 and a2 != 0:
         y = -c1 / b1
         x = (b2*c1/b1 - c2) / a2
